20abd4010791df01fdd776a9a77ecd7dd99514b4
Abdul Kadir Khan <abdulkadirk059@gmail.com>
2023-12-20 10:33:13 +0530

    first commit

---
+++ js/scheduleSheet.js
@@ -0,0 +1,70 @@
+// ------------------------------------------------------------------------------------------------------------
+//getting the data from google sheet reusable (multiple) table
+
+const getDataFromGoogleSheets2 = (SHEET_ID,RANGE,SHEET_TITLE,tableNo) => {
+  const GOOGLE_SHEET_URL = `https://docs.google.com/spreadsheets/d/${SHEET_ID}/gviz/tq?sheet=${SHEET_TITLE}&range=${RANGE}`;
+
+  fetch(GOOGLE_SHEET_URL)
+    .then((response) => response.text())
+    .then((data) => {
+      const jsonStart = data.indexOf("google.visualization.Query.setResponse(");
+
+      if (jsonStart !== -1) {
+        const jsonString = data.substring(
+          jsonStart + "google.visualization.Query.setResponse(".length,
+          data.length - 2
+        );
+
+        const jsonData = JSON.parse(jsonString);
+        const dataTable = jsonData.table;
+        const rows = dataTable.rows;
+
+        const tableBody = document.getElementById("indianTableBody"+tableNo);
+        let timeFlag = true;
+
+        console.log('rows...KadirKhan...',rows);
+
+        rows.forEach((rowData) => {
+          console.log("rowData..??", rowData);
+          const row = document.createElement("tr");
+          rowData.c.forEach((cellData, index) => {
+            const cell = document.createElement("td");
+            const link = document.createElement("a");
+            if ((index === 3 && timeFlag)) {
+              cell.textContent = cellData.v;
+              cell.setAttribute("rowspan", 3);
+              row.appendChild(cell);
+               
+              timeFlag = false;
+            }
+            else if(index === 0 || index === 2){
+              link.textContent = cellData?.v;
+              // link.setAttribute("href","https://abdulkadirkhan786.netlify.app/");
+              let id = cellData?.v;
+              // console.log('id...???',id);
+              // console.log('id...typeof',typeof id);
+              link.setAttribute("href",`http://www.kktv1.com/m/?roomid=${id}`);   // here i am updating the user id dynamically...
+              cell.appendChild(link);
+              row.appendChild(cell);
+            }else if (index !== 3) {
+              cell.textContent = cellData?.v;
+              row.appendChild(cell);
+            }
+          });
+
+          tableBody.appendChild(row);
+        });
+        const tableContainer = document.getElementById("indiaTable"+tableNo);
+        tableContainer.appendChild(tableBody);
+      } else {
+        console.error("Data not found in the response.");
+      }
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+    });
+}
+
+getDataFromGoogleSheets2("11ZLpOSrhZISYYzGusflZZA3FcXkznRKfvaO_t9hzKJA","B33:E35","Dec 23 Agency PK Battle","1");
+getDataFromGoogleSheets2("11ZLpOSrhZISYYzGusflZZA3FcXkznRKfvaO_t9hzKJA","B37:E39","Dec 23 Agency PK Battle","2");
+getDataFromGoogleSheets2("11ZLpOSrhZISYYzGusflZZA3FcXkznRKfvaO_t9hzKJA","B41:E43","Dec 23 Agency PK Battle","3");

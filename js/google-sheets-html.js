50050b40ab8d5ca37362371fca0370ff0ec611ec
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-04-25 15:24:59 +0530

    leaderboard round1 has been updated

--- js/google-sheets-html.js
+++ js/google-sheets-html.js
@@ -15,7 +15,7 @@ google.load('visualization', '1', {
 var visualization;
 
 function drawVisualization() {
-    var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1ZCLMDdiPueCkjpWdfPEieSM6nt4js1CKly2wUEjKasI/edit?usp=sharing');
+    var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1CdkNmdeY1UdHPnXZ4CcLfqJoVErZJlfWKSIE5lX-IGc/edit#gid=0');
     console.log(query,'quer>>>>y')
     query.setQuery('SELECT A, B, C, D, E label A "Id", B "Timings", C "TALENT Name", D "Category", E  "Date"');
     query.send(handleQueryResponse);
@@ -111,11 +111,11 @@ function rowsCount(item){
  
   dataList.forEach(dataCount);
   console.log("counter is"+counter);
-  if(counter>=0   &&  counter<15){
+  if(counter>=0   &&  counter<7){
      $('#conDiv').append('<div class="colThree"><div><a href="http://www.kktv1.com/m/?roomid='+id+'"><img src="../common/img/kk_head_avatar_men.png" id="u_img_'+id+'" alt="" class="imgClass"></a></div><div class="centerDiv"><img src="img/coin.png" class="imgsmal">'+name+'<br>ID-'+id+'</div><div class="lastDiv"><span class="dancing">'+times+'</span></div></div>');
 
   }
-  else  if(counter>=15  &&  counter<26){
+  else  if(counter>=7  &&  counter<12){
 
 
   
@@ -125,7 +125,7 @@ function rowsCount(item){
 
   }
 
-  else   if(counter>=26  &&  counter<=32){
+  else   if(counter>=12  &&  counter<=13){
 $('#agencyrec').append('<div class="colThreelast"><div>Id: '+id+'</div><div class="centerDivlast"><img src="img/coin.png" class="imgsmal">'+name+'<br></div><div class="lastDiv"><span class="dancing">'+times+'</span><p></p></div></div>');
 
  
